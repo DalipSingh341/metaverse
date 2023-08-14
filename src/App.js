5a8d2649b35ed682b58573c7491494fbcd540d89
Dipakkkk <dr916139@gmail.com>
2023-08-14 13:57:07 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Instagram from "./components/assets/svg/instagram"
+import Discord from "./components/assets/svg/Discord"
+import Linkdin from "./components/assets/svg/Linkdin"
+import Twitter from "./components/assets/svg/Twitter"
+import Youtube from "./components/assets/svg/Youtube"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='d-flex'>
+      <Instagram />
+      <Discord />
+      <Linkdin />
+      <Twitter />
+      <Youtube/>
     </div>
   );
 }
